50cff885284b45ed6f8f13f028546a8e0a6101a6
PyWendi <rakotondranaivogilbert21@gmail.com>
2023-05-06 23:49:23 +0300

    chatGPT API implementation

--- Api/src/controllers/chat.controller.js
+++ Api/src/controllers/chat.controller.js
@@ -1,5 +1,20 @@
 const user = require('../models/user.model')
 const chat = require('../models/chat.model')
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+/**
+ * We import open ai module and configure it to run the API
+ */
+const { Configuration, OpenAIApi } = require("openai")
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
 
 
 /* 
@@ -40,19 +55,42 @@ exports.allChat = (req, res) =>  {
 /* 
     Create a chat for an authenticated user
 */
-exports.createChat = (req, res) => {
+exports.createChat = async (req, res) => {
 
     // Getting user ID and data from frontend
     const userId = req.cookies.user
-    const { theme, user_text, bot_text } = req.body
+    const { theme, user_text } = req.body
+    let reponse = ""
 
-    if(theme != '' && user_text != '' && bot_text != '')
+    user_text = user_text + "\n\n Context : analyse de reve \n Relation du reve avec l'avenir."
+
+    if(theme != '' && user_text != '')
     {
+
+        /**
+         * Here we send the prompt to the openai api and register back the data from it and 
+         * send it back to the frontend app.
+         */
+        try {
+            const completion = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages:[{role : 'user' , content: user_text}],
+            });
+            reponse = completion.data.choices[0].message.content;
+    
+        } catch (error) {
+            res.status(400)
+            .json({
+                error : true,
+            })
+        }
+
+
         // Create a chat for 
         const newChate = new chat({
             theme: theme,
             user_text: user_text,
-            bot_text: bot_text,
+            bot_text: reponse,
             owner: userId
         })
 
@@ -60,7 +98,10 @@ exports.createChat = (req, res) => {
         
         res
         .status(201)
-        .end()
+        .json({
+            error: false,
+            reponse : reponse
+        })
     }
     else{
         res
@@ -125,4 +166,29 @@ exports.delete = (req, res) => {
     chat.deleteOne({_id: idChat})
     .then((result)=>{res.status(201).end()})
     .catch((err)=>{res.status(400).json({error: err})})
+}
+
+
+/**
+ * ChatGPT API test
+ */
+
+exports.chatgpt = async (request, response) => {
+    const prompt = request.body.prompt
+
+    try {
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages:[{role : 'user' , content: prompt}],
+        });
+        console.log(completion.data.choices[0].message.content);
+
+    } catch (error) {
+        if (error.response) {
+          console.log(error.response.status);
+          console.log(error.response.data);
+        } else {
+          console.log(error.message);
+        }
+    }
 }
--- Api/src/routes/chat.route.js
+++ Api/src/routes/chat.route.js
@@ -1,9 +1,10 @@
 const route = require('express').Router()
-const chatController = require('../controllers/chat.controller')
+const chatController = require('../controllers/chat.controller.js')
 
 route.get('/all', chatController.allChat)
 route.post('/create', chatController.createChat)
 route.put('/change', chatController.modify)
 route.delete('/delete', chatController.delete)
+route.post('/chatgpt_api', chatController.chatgpt)
 
 module.exports = route
