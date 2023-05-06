1a75d0d39ce5359dcde16405af1a0650a749073a
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-05-07 01:59:56 +0300

    ajout de description

--- client/src/featurs/Acceuille/Components/Advantage/Advantage.jsx
+++ client/src/featurs/Acceuille/Components/Advantage/Advantage.jsx
@@ -21,24 +21,42 @@ function Advantage() {
 ]
   return (
     <div className='bg-[#09081A]'>
-        <div className=" flex justify-between items-center p-10">
+        <div className=" flex justify-between p-10">
             <div className="grid grid-cols-2 gap-y-8 gap-x-5 w-[45%] ">
                 {
                     table.map((elem,key)=>(
-                    <div key={key} className="bg-[#251B3C] flex flex-col justify-center items-center">
-                        <div className=" bg-[#D9D9D9] "/>
+                    <div key={key} className="bg-[#251B3C] flex flex-col justify-center items-center p-6 text-center rounded-lg">
+                        <div className=" bg-[#D9D9D9] h-20 w-36  "/>
                         <h3 className='h3 text-[#16F2F2] font-semibold'>{elem.title}</h3>
-                        <p className='text-white font-Exo2'>
+                        <p className='text-white font-Exo2 text-sm'>
                             {elem.describe}
                         </p>
                     </div>
                     ))
                 }
             </div>
-            <div className="">
-                <h2 className='h2 text-white font-bold'>
-                    LOREM SIT DOLOR SIT DAK SUM LEE MI NOH 
-                </h2>
+            <div className="w-[50%] flex flex-col justify-between   ">
+                <div className=" flex flex-col space-y-5">
+                    <h2 className='h2 text-white font-bold'>
+                        LOREM SIT DOLOR SIT DAK SUM LEE MI NOH 
+                    </h2>
+                    <p className='text-white font-Exo2'>
+                        Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, 
+                        ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent
+                         per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                    </p>
+                    <button className='flex justify-center items-center h-[6vh] w-[16vw] font-Exo2 bg-[#C525EA] text-white font-bold'>
+                        TRY IT FO FREE 
+                    </button>
+                </div>
+                <div className="flex space-y-3 flex-col">
+                    <h3 className='h3 text-white' >Follow us:</h3>
+                    <div className=" flex space-x-2">
+                        <div className="bg-[#C525EA] rounded-full w-8 h-8"></div>
+                        <div className="bg-[#C525EA] rounded-full w-8 h-8"></div>
+                        <div className="bg-[#C525EA] rounded-full w-8 h-8"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- client/src/featurs/Acceuille/Components/Transition/Transition.jsx
+++ client/src/featurs/Acceuille/Components/Transition/Transition.jsx
@@ -3,7 +3,7 @@ import Blend from '../../../../assets/Blend2.svg'
 
 function Transition() {
   return (
-    <div className='bg-[#251B3C] h-[53vh] overflow-hidden text-center relative flex flex-col  items-center '>
+    <div className='bg-[#251B3C] h-[55vh] overflow-hidden text-center relative flex flex-col  items-center '>
        <div className="flex flex-col space-y-6 w-[70%] mt-12 ">
             <h2 className='h2 text-[#16F2F2] font-bold uppercase'> Lorem Ipsum Dolor Sit Col dolor </h2>
             <p className='font-Exo2 text-white'> 
@@ -13,7 +13,7 @@ function Transition() {
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium 
             </p>
        </div>
-        <img src={Blend} alt='blend' className='absolute  left-0' />
+        <img src={Blend} alt='blend' className='absolute  left-0 h-[70vh]' />
     </div>
   )
 }
