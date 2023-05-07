53b33b7d4a3d8d490a1c478ec46c58cc4bb520b7
niriantsoa <niriantsoa@pop-os.localdomain>
2023-05-07 10:32:30 +0300

    Bug correction

--- client/src/featurs/Acceuille/Components/Organisation/Organisation.jsx
+++ client/src/featurs/Acceuille/Components/Organisation/Organisation.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import slide1 from './../../../../assets/slide1.jpg'
+import slide2 from './../../../../assets/slide2.png'
+import slide3 from './../../../../assets/slide3.jpeg'
 
- 
+
+
 function Organisation() {
     const slide = (
-        <AwesomeSlider className='w-full h-[screen]' >  
-            <div className='bg-[rgb(214,162,18)] h-full w-[30vw] text-[#040404] rounded-lg'>1</div>
-            <div className='bg-[#efefef] h-full w-[30vw] text-[#040404] rounded-lg'>2</div>
-            <div className='bg-red-500 h-full w-[30vw] text-[#040404] rounded-lg'>3</div>
+        <AwesomeSlider className='w-[60%] h-[75vh]' >
+            <div className='h-full w-[50vw] text-[#040404] rounded-lg '><img src={slide1} className='min-w-full min-h-full' alt="" /></div>
+            <div className='h-full w-[50vw] text-[#040404] rounded-lg '><img src={slide2} className='min-w-full min-h-full' alt="" /></div>
+            <div className='h-full w-[50vw] text-[#040404] rounded-lg '><img src={slide3} className='min-w-full min-h-full' alt="" /></div>
         </AwesomeSlider>
     )
   return (
@@ -22,12 +26,13 @@ function Organisation() {
                     <h3>WHO IS INTERNATIONAL INSTITUTE  OF DREAMS?</h3>
                 </div>
             </div>
-            <div className="text-white font-Exo2 flex flex-col space-y-3">
-                <p>Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
-                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-                <p>
-                Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet 
-                odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            <div className="text-white org-cont-p font-Exo2">
+                <p className='org-p'>The International Institute of dreams is a multidisciplinary organization dedicated 
+                to the pure and applied investigation of dreams, dreaming and nightmares.
+                </p>
+                <p className='org-p'>
+                    We've developped Onirix to help people to know what their dreams tell them about them and their future, 
+                    love story and many useful informations.
                 </p>
             </div>
             
