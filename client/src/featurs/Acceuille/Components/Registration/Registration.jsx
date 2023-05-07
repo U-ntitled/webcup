ba76efe1489f4f8d4812c1002ae09adfb39564d8
Antonino <ninohantonio@gmail.com>
2023-05-07 08:13:31 +0300

    registration

--- client/src/featurs/Acceuille/Components/Registration/Registration.jsx
+++ client/src/featurs/Acceuille/Components/Registration/Registration.jsx
@@ -5,8 +5,8 @@ function Registration() {
   return (
     <div className='h-[85vh] text-center pt-[14vh] bg-[#25183C]'>
 
-        <h2 className = 'h3 text-[2.1em] font-Exo2 text-[white] font-bold RG-center mb-[10vh]'>REGISTER FOR <span className = "text-[#16F2F2]" > FREE </span> AND GET TO KNOW  <br />WHAT YOUR <span className = "text-[#16F2F2]" > DREAMS </span> MEANS</h2>
-        <h4 className='text-[1em] font-Exo2 text-[white] font-bold'>
+        <h2 className = 'h3 text-[2.6em] font-Exo2 text-[white] font-bold RG-center mb-[10vh]'>REGISTER FOR <span className = "text-[#16F2F2]" > FREE </span> AND GET TO KNOW  <br />WHAT YOUR <span className = "text-[#16F2F2]" > DREAMS </span> MEANS</h2>
+        <h4 className='text-[1em] font-Exo2 font-bold text-[white]'>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error quia corporis veritatis magnam magni rerum maiores et odio harum repudiandae. <br/>
           lorem ipsum dolor sit amet consectetur adipscing elit
         </h4>
