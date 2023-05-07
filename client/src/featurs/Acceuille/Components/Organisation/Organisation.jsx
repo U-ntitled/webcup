a8668c6c7dfa3d261cb64ab7e6df518bbd5e6915
Antonino <ninohantonio@gmail.com>
2023-05-07 08:27:41 +0300

    registration

--- client/src/featurs/Acceuille/Components/Organisation/Organisation.jsx
+++ client/src/featurs/Acceuille/Components/Organisation/Organisation.jsx
@@ -22,23 +22,24 @@ function Organisation() {
             <div className='h3 text-[#C525EA]'>
                 <h3>WHO IS INTERNATIONAL INSTITUTE  OF DREAMS?</h3>
             </div>
-<<<<<<< HEAD
+
             <div className="text-white font-Exo2 flex flex-col space-y-3">
                 <p>Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                     Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-=======
-            <div className="text-white font-Exo2">
-                <p>The International Institute of dreams is a multidisciplinary organization dedicated 
-                to the pure and applied investigation of dreams, dreaming and nightmares. Our purposes are to promote an awareness 
-                and appreciation of dreams in both professional and public arenas; to encourage research into the nature, function, 
-                and significance of dreaming; to advance the application of the study of dreams.
-                </p>
->>>>>>> a2f3378 (Phone responsive for the hero section)
-                <p>
-                    We've developped Onirix to help people to know what their dreams tell them about them and their future, 
-                    love story and many useful informations.
-                </p>
             </div>
+
+                <div className="text-white font-Exo2">
+                    <p>The International Institute of dreams is a multidisciplinary organization dedicated 
+                    to the pure and applied investigation of dreams, dreaming and nightmares. Our purposes are to promote an awareness 
+                    and appreciation of dreams in both professional and public arenas; to encourage research into the nature, function, 
+                    and significance of dreaming; to advance the application of the study of dreams.
+                    </p>
+                    <p>
+                        We've developped Onirix to help people to know what their dreams tell them about them and their future, 
+                        love story and many useful informations.
+                    </p>
+                </div>
+            
             <div className="font-Exo2 font-normal">
                 <button className='flex justify-center x items-center h-[6vh] w-[16vw] font-Exo2 bg-white  font-bold'>More about us</button>
             </div>
