58feaffff460c5b041c51f4101e912c524142366
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-05-07 07:32:07 +0300

    finalisation de l' acceuille

--- client/src/featurs/Acceuille/Components/Transition/Transition.jsx
+++ client/src/featurs/Acceuille/Components/Transition/Transition.jsx
@@ -6,7 +6,7 @@ function Transition() {
     <div className='bg-[#251B3C] h-[55vh] overflow-hidden text-center relative flex flex-col  items-center '>
        <div className="flex flex-col space-y-6 w-[70%] mt-12 ">
             <h2 className='h2 text-[#16F2F2] font-Exo2 trans-title font-bold uppercase'> THE RISE OF ARTIFICIAL INTELLIGENCE </h2>
-            <p className='font-Exo2 text-white trans-p'> 
+            <p className='font-Exo2 text-white trans-p'>
               Nowadays, artificial intelligences are making a huge revolution to into our society. 
               They have an overwhelming and very useful capacity almost in every domains... 
               There are plenty of AI assistants that have been developped at the moment.
