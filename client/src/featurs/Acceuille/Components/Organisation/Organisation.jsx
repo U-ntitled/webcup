bd95514bbc982c9507cc7b8be25837a773236615
niriantsoa <niriantsoa@pop-os.localdomain>
2023-05-07 08:37:32 +0300

    Adding datas to slides

--- client/src/featurs/Acceuille/Components/Organisation/Organisation.jsx
+++ client/src/featurs/Acceuille/Components/Organisation/Organisation.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
-<<<<<<< HEAD
- 
-=======
 import slide1 from './../../../../assets/slide1.jpg'
 import slide2 from './../../../../assets/slide2.png'
 import slide3 from './../../../../assets/slide3.jpeg'
 
 
 
->>>>>>> 5db434e (Adding data to slides)
 function Organisation() {
     const slide = (
         <AwesomeSlider className='w-[60%] h-[67vh]' >
@@ -28,12 +24,6 @@ function Organisation() {
             <div className='h3 text-[#C525EA]'>
                 <h3>WHO IS INTERNATIONAL INSTITUTE  OF DREAMS?</h3>
             </div>
-<<<<<<< HEAD
-
-            <div className="text-white font-Exo2 flex flex-col space-y-3">
-                <p>Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
-                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-=======
             <div className="text-white font-Exo2">
                 <p>The International Institute of dreams is a multidisciplinary organization dedicated 
                 to the pure and applied investigation of dreams, dreaming and nightmares.
@@ -42,20 +32,7 @@ function Organisation() {
                     We've developped Onirix to help people to know what their dreams tell them about them and their future, 
                     love story and many useful informations.
                 </p>
->>>>>>> 5db434e (Adding data to slides)
             </div>
-
-                <div className="text-white font-Exo2">
-                    <p>The International Institute of dreams is a multidisciplinary organization dedicated 
-                    to the pure and applied investigation of dreams, dreaming and nightmares. Our purposes are to promote an awareness 
-                    and appreciation of dreams in both professional and public arenas; to encourage research into the nature, function, 
-                    and significance of dreaming; to advance the application of the study of dreams.
-                    </p>
-                    <p>
-                        We've developped Onirix to help people to know what their dreams tell them about them and their future, 
-                        love story and many useful informations.
-                    </p>
-                </div>
             
             <div className="font-Exo2 font-normal">
                 <button className='flex justify-center x items-center h-[6vh] w-[16vw] font-Exo2 bg-white  font-bold'>More about us</button>
