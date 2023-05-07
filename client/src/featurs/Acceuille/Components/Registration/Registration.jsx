0c31d27b9fbca987396167dfbff966f080fb4fdf
Antonino <ninohantonio@gmail.com>
2023-05-07 07:54:57 +0300

    registration

--- client/src/featurs/Acceuille/Components/Registration/Registration.jsx
+++ client/src/featurs/Acceuille/Components/Registration/Registration.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './registre.module.css'
 
 function Registration() {
   return (
-    <div>Registration</div>
+    <div className='h-[85vh] text-center pt-[14vh] bg-[#25183C]'>
+
+        <h2 className = 'h3 text-[2.1em] font-Exo2 text-[white] font-bold RG-center mb-[10vh]'>REGISTER FOR <span className = "text-[#16F2F2]" > FREE </span> AND GET TO KNOW  <br />WHAT YOUR <span className = "text-[#16F2F2]" > DREAMS </span> MEANS</h2>
+        <h4 className='text-[1em] font-Exo2 text-[white] font-bold'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error quia corporis veritatis magnam magni rerum maiores et odio harum repudiandae. <br/>
+          lorem ipsum dolor sit amet consectetur adipscing elit
+        </h4>
+        <button className='btn RG-bouton bg-[#c525EA] w-[20vw]  h-[7vh]'>
+          TRY IT FOR FREE
+        </button>
+
+
+    </div>
   )
 }
 
